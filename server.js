14ec2db4070b25e3061b85421bdc216feea78c90
Jaber Ahmed <70724851+JaberAhmed33@users.noreply.github.com>
2023-04-21 21:26:35 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 7000;
 const http = require("http").createServer(app);
 const io = require("socket.io")(http, {
   cors: {
-    origin: process.env.CLIENT_URL,
+    origin: "https://facedook-lilac.vercel.app",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-type"],
   },
